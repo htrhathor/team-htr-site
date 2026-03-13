b3c2f5211d151ac98c629c449a8ec946bd9d766f
htrhathor <htrhathor.pro@gmail.com>
2026-03-13 17:23:13 +0100

    Update api.py

--- api.py
+++ api.py
@@ -295,10 +295,14 @@ def live():
 # ======================================
 
 if __name__ == "__main__":
-    import os
+    print("API HTR démarrée")
+    # Récupère le port de Render ou Heroku
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
     port = int(os.environ.get("PORT", 5000))  # prend le port de Render ou 5000 par défaut
     print(f"API HTR démarrée sur le port {port}")
 
     # écoute sur toutes les interfaces pour que ce soit accessible publiquement
     app.run(host="0.0.0.0", port=port)
+
